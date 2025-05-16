24f060844038085acba2ba1dce32f8d59c2aaa40
Pierre Bertrand <pierre.bertrand.professionnel@gmail.com>
2025-05-16 12:57:00 +0200

    fix(home): update template homepage

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,10 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 import TheHeader from './components/Layout/TheHeader.vue'
 </script>
 
 <template>
   <TheHeader />
-  <div class="content">
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </div>
-
-  <RouterView />
+  <main>
+    <RouterView />
+  </main>
 </template>
-
-<style scoped>
-.content {
-  line-height: 1.5;
-  height: 80vh;
-  width: 50%;
-  float: left;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,33 @@
 <template>
-  <main>
+  <div class="content">
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </div>
+  <section class="main">
     <h1 class="green">Welcome to Vue 3 + Vite</h1>
-  </main>
+  </section>
 </template>
 
+<script setup>
+import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
 <style scoped>
-main {
+.content {
+  line-height: 1.5;
+  height: 80vh;
+  width: 50%;
+  float: left;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.main {
   display: flex;
   justify-content: center;
   align-items: center;
