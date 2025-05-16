9e5eea17963a262fe0b61cf40c970f2543a7b0c2
Pierre Bertrand <pierre.bertrand.professionnel@gmail.com>
2025-05-16 10:48:01 +0200

    Initial commit

---
+++ src/components/Layout/TheHeader.vue
@@ -0,0 +1,14 @@
+<template>
+    <header class="navbar">
+        <nav class="navbar-content container">
+            <RouterLink to="/" class="navbar-logo">
+                <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="80" height="80" />
+            </RouterLink>
+            <ul class="navbar-links">
+                <li>
+                    <RouterLink to="/">Home</RouterLink>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <h1 class="green">Welcome to Vue 3 + Vite</h1>
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+  width: 50%;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
